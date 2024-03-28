db30361427e95650ed4177fd059a29d62355376d
unknown <ajshakthivelu@gmail.com>
2024-03-29 01:58:28 +0530

    constansts of card

---
+++ frontend/src/constants/mianScreen.jsx
@@ -0,0 +1,27 @@
+const cardss =[
+    {
+        '_id':'1',
+        'name':'CAMERA',
+        'image':'/images/search.jpeg',
+        'content':' Simply snap a photo of your ingredients using our websites built-in camera functionality, and within seconds, discover a variety of delicious recipes tailored to your ingredients.'
+
+    },
+    {
+        '_id':'2',
+        'name':'INGREDIENTS',
+        'image':'/images/ingidients.jpeg',
+        'content':'Simply type out the ingredients you have on hand into our website, and instantly unlock a world of culinary possibilities. From pantry staples to fresh produce, our intelligent search algorithm matches your ingredients with a wide array of recipes, ensuring you never run out of meal ideas.'
+    },
+    {
+        '_id':'3',
+        'name':'NUTRITION',
+        'image':'/images/nutrition.jpeg',
+        'content':'With our websites intuitive interface, you can effortlessly monitor your daily intake of key nutrients like proteins and calories. Set your nutritional targets, track your progress in real-time, and stay motivated to reach your health and fitness goals.'
+    },
+    {
+        '_id':'4',
+        'name':'RANDOM',
+        'image':'/images/random.jpeg',
+        'content':'Discover culinary inspiration at the click of a button with our random recipe generator! Whether your feeling adventurous in the kitchen or simply seeking fresh meal ideas, our website delivers a diverse selection of recipes at your fingertips.'
+    }
+]
