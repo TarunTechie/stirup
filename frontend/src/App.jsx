8c088f348e8114d04c3f467dd8d17f85775dbba7
tarun-techie <taruntechie25@gmail.com>
2024-03-29 02:22:36 +0530

    added home page and features section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,15 @@
 import './App.css'
 import './index.css'
-import Card from './components/card'
 import Top from './components/top'
+import Features from './components/features'
 function App() {
   return (
     <>
     <Top/>
-    <div>
-      <img src="/images/frontpage.png" alt="" srcset="" />
+    <div className='mb-8'>
+      <img src="/images/frontpage.png" className='w-screen h-screen '/>
     </div>
-    <div className='flex'>
-    <Card
-    image={"/images/random.jpeg"}
-    name={"RANDOM"}
-    content={"this card will be genetating random recepies in the card so ya this is a card sdysdfhdfhdrejertsejyerjyethestetyheyeye"}
-    />
-    </div>
+    <Features/>
     </>
   )
 }
