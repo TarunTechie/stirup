ef186f455abf9b14226703cfab0c32ca71fb0388
tarun-techie <taruntechie25@gmail.com>
2024-03-30 03:09:40 +0530

    search done

--- frontend/src/components/searchBar.jsx
+++ frontend/src/components/searchBar.jsx
@@ -1,59 +1,30 @@
 import React,{useState,useEffect} from "react"
 import axiosInstance from "../constants/spoon"
 
-const apiKey = '9285ed5ac3734555ace89246806efe2f';
 function SearchBar(){
-
-    const [query, setQuery] = useState('');
-    const [suggestionsRec, setSuggestionsRecipes] = useState([]);
-    const [suggestionsIng, setSuggestionsIngridients] = useState([]);
-  
-  
-      const fetchSuggestions = async (event) => {
-        event.preventDefault();
-
-    if (query.trim() === '') {
-      setSuggestionsRecipes([]);
-      setSuggestionsIngridients([]);
-      return;
+  const [food,setfood]=useState('')
+  const send=async ()=>
+  {
+    console.log(food)
+    try{
+      const result=await axiosInstance.get('food/search',{params:{'query':food,'number':5}})
+      console.log(result.data.searchResults[0].results)
     }
-        try {
-          const response = await fetch(`https://api.spoonacular.com/food/search?apiKey=${apiKey}&query=${query}`);
-          const data = await response.json();
-          setSuggestionsRecipes(data.searchResults[0].results);
-          setSuggestionsIngridients(data.searchResults[1].results);
-          console.log(suggestionsRec)
-          console.log(suggestionsIng) // Limiting to 5 suggestions
-        } catch (error) {
-          console.error('Error fetching suggestions:', error);
-        }
-      };
-  
-     
-      
-
-    const handleChange = (event) => {
-      setQuery(event.target.value);
-
-    };
-  
-    const handleSelectSuggestion = (suggestion) => {
-      setQuery(suggestion.name);
-      setSuggestionsRecipes([]);
-      setSuggestionsIngridients([]);
-    };
-
-
-
-
-  return(     <div class="flex mt-20 pt-2 relative mx-auto justify-center items-center">
-    <form onSubmit={fetchSuggestions}>
-  <input class="border-2 border-maron bg-white h-10 w-4/6 px-5 pr-16 rounded-3xl text-2xl tracking-widest focus:outline-orange"
-    type="search" name="search" placeholder="Find a dish or ingridient" value={query} onChange={handleChange}/>
-  <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
+    catch(error){console.error(error)}
+  }
+  const change=(event)=>
+  {
+    event.preventDefault();
+    setfood(event.target.value)
+  }
+  return(     <div className="flex mt-20 p-4 relative mx-auto justify-center">
+  <div className="flex w-4/6 p-4 bg-white border-maron border-2 rounded-3xl justify-around  focus:outline-orange">
+  <input className="bg-white h-10 w-4/6 px-5 pr-16 rounded-3xl text-2xl tracking-widest focus:outline-none"
+    type="search" name="search" placeholder="Find a dish or ingridient" onChange={change}/>
+  <button onClick={send}>
   <img src="\icons\spoon-svgrepo-com.svg" alt="" srcset="" className="w-10 h-10"/>
   </button>
-  </form>
+  </div>
 </div>)
 
 }
--- frontend/src/constants/spoon.jsx
+++ frontend/src/constants/spoon.jsx
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
-const apiKey = '9285ed5ac3734555ace89246806efe2f';
-
 const axiosInstance = axios.create({
-  baseURL: 'https://api.spoonacular.com',
+  baseURL: 'https://api.spoonacular.com/',
   headers: {
+    'x-api-key': '9285ed5ac3734555ace89246806efe2f',
     'Content-Type': 'application/json',
   },
-  params:{
-    apiKey: apiKey
-}
 });
 
 export default axiosInstance;
