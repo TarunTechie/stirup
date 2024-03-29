6590468a0952cf8cca530e41be9eedfd101703b1
GommaVainkoo <ajshakthivelu@gmail.com>
2024-03-30 01:58:32 +0530

    search bar

--- frontend/src/components/searchBar.jsx
+++ frontend/src/components/searchBar.jsx
@@ -1,53 +1,59 @@
 import React,{useState,useEffect} from "react"
 import axiosInstance from "../constants/spoon"
 
-
+const apiKey = '9285ed5ac3734555ace89246806efe2f';
 function SearchBar(){
 
     const [query, setQuery] = useState('');
-    const [suggestions, setSuggestions] = useState([]);
+    const [suggestionsRec, setSuggestionsRecipes] = useState([]);
+    const [suggestionsIng, setSuggestionsIngridients] = useState([]);
   
-    useEffect(() => {
-      if (query.trim() === '') {
-        setSuggestions([]);
-        return;
-      }
   
-      const fetchSuggestions = async () => {
+      const fetchSuggestions = async (event) => {
+        event.preventDefault();
+
+    if (query.trim() === '') {
+      setSuggestionsRecipes([]);
+      setSuggestionsIngridients([]);
+      return;
+    }
         try {
-          const response = await axiosInstance.get('/food/search?query=${query}');
+          const response = await fetch(`https://api.spoonacular.com/food/search?apiKey=${apiKey}&query=${query}`);
           const data = await response.json();
-          setSuggestions(data.results.slice(0, 5));
-          console.log(suggestions) // Limiting to 5 suggestions
+          setSuggestionsRecipes(data.searchResults[0].results);
+          setSuggestionsIngridients(data.searchResults[1].results);
+          console.log(suggestionsRec)
+          console.log(suggestionsIng) // Limiting to 5 suggestions
         } catch (error) {
           console.error('Error fetching suggestions:', error);
         }
       };
   
-      fetchSuggestions();
-  
+     
       
-      return () => setSuggestions([]);
-    }, [query]);
-  
+
     const handleChange = (event) => {
       setQuery(event.target.value);
+
     };
   
     const handleSelectSuggestion = (suggestion) => {
       setQuery(suggestion.name);
-      setSuggestions([]);
+      setSuggestionsRecipes([]);
+      setSuggestionsIngridients([]);
     };
 
 
 
 
   return(     <div class="flex mt-20 pt-2 relative mx-auto justify-center items-center">
+    <form onSubmit={fetchSuggestions}>
   <input class="border-2 border-maron bg-white h-10 w-4/6 px-5 pr-16 rounded-3xl text-2xl tracking-widest focus:outline-orange"
     type="search" name="search" placeholder="Find a dish or ingridient" value={query} onChange={handleChange}/>
   <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
   <img src="\icons\spoon-svgrepo-com.svg" alt="" srcset="" className="w-10 h-10"/>
   </button>
+  </form>
 </div>)
 
 }
