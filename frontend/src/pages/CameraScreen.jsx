d06c32787374c49ce67a5a0c2e7eb5a578945fed
GommaVainkoo <ajshakthivelu@gmail.com>
2024-04-11 09:20:02 +0530

    add button fuctionality

--- frontend/src/pages/CameraScreen.jsx
+++ frontend/src/pages/CameraScreen.jsx
@@ -1,6 +1,32 @@
 import Top from "../components/top"
+import Bottom from "../components/bottom";
+import { useState } from "react";
 
 function CameraScreen (){
+    const [ingredients, setIngredients] = useState(['']);
+  
+    const handleAddInput = () => {
+      const Ingredients = [...ingredients];
+      if(Ingredients.length<6){
+        setIngredients([...ingredients, '']);
+      }
+      
+    };
+  
+    const handleDeleteInput = (index) => {
+      const updatedIngredients = [...ingredients];
+      if(updatedIngredients.length>1){
+        updatedIngredients.splice(index, 1);
+        setIngredients(updatedIngredients);
+      }
+      
+    };
+  
+    const handleInputChange = (index, event) => {
+      const updatedIngredients = [...ingredients];
+      updatedIngredients[index] = event.target.value;
+      setIngredients(updatedIngredients);
+    };
 return(
     <div className="bg-white">
     <Top />
@@ -8,6 +34,22 @@ return(
         <img src="/images/pagebg.jpg" className="w-screen" />
     </div>
     <h1 className="font-heading text-7xl ml-24 underline decoration-orange decoration-2 underline-offset-[12px] ">Snap and Cook</h1>
+    <h1 className="font-light text-5xl ml-36">Ingredients</h1>
+    <button className="btn" onClick={handleAddInput}>Add </button>
+    {ingredients.map((ingredient, index) => (
+         <div key={index}>
+         <input
+           type="text"
+           value={ingredient}
+           onChange={(event) => handleInputChange(index, event)}
+           placeholder="Enter ingredient"
+         />
+         <button onClick={() => handleDeleteInput(index)}><span><img src="/icons/cross.svg" className="w-8 h-8"/></span></button>
+         </div>
+          
+    ))}
+    <button className="lrbtn">Search Recipe</button>
+    <Bottom />
     </div>
 )
 }
