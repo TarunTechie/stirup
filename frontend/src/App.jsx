d39b251f3e50a9aea27423085189f1fa96ddde86
tarun-techie <taruntechie25@gmail.com>
2024-08-15 15:24:26 +0530

    minor bug fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
 import './index.css'
-import HomeScreen from './pages/HomeScreen'
-import LoginScreen from './pages/LoginScreen'
 function App() {
   return (
     <div>
--- frontend/src/constants/spoon.jsx
+++ frontend/src/constants/spoon.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const spoon = axios.create({
   baseURL: 'https://api.spoonacular.com/',
   headers: {
-    'x-api-key': '9285ed5ac3734555ace89246806efe2f',
+    'x-api-key':import.meta.env.VITE_APIKEY ,
     'Content-Type': 'application/json',
   },
 });
